f658cdaabaec5722c90a6b73f8d5e63bc65231b5
Michael Felix <mikemh@uri.edu>
2024-05-21 09:57:49 -0400

    merge CommonLibSSE-NG

---
+++ cmake/testlist.cmake
@@ -0,0 +1,8 @@
+set(TESTS
+    tests/Main.cpp
+    tests/RE/A/ActorValues.test.cpp
+    tests/RE/F/FormTypes.test.cpp
+    tests/REL/Relocation.test.cpp
+    tests/SKSE/Interfaces.test.cpp
+    tests/SKSE/Trampoline.test.cpp
+)
