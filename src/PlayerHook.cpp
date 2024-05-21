37c7288f62ceb0807365ecfa3634e9c7c6baf875
Michael Felix <mikemh@uri.edu>
2024-05-21 15:32:59 -0400

    a

--- src/PlayerHook.cpp
+++ src/PlayerHook.cpp
@@ -1,25 +1,7 @@
-#pragma once
-
-#include <SKSE/SKSE.h>
-#include <RE/Skyrim.h>
-#include <REL/Relocation.h>
-#include <SKSE/Trampoline.h>
+#include "PlayerHook.h"
+#include <spdlog/spdlog.h>
 
 namespace Hooks
-{
-	class PlayerHook
-	{
-	public:
-		static void Hook();
-
-	private:
-		static void HookedShoutFunction(int64_t* param_1, int param_2);
-		static inline REL::Relocation<decltype(HookedShoutFunction)> _ShoutFunction;
-	};
-}#include "PlayerHook.h"
-#include <spdlog/spdlog.h>
-	
-	namespace Hooks
 {
 	void PlayerHook::Hook()
 	{
