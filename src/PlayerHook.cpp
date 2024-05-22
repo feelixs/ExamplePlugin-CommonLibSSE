8c3a07488bfd89d009752ec67ee74708787efd83
Michael Felix <mikemh@uri.edu>
2024-05-22 11:35:20 -0400

    Update PlayerHook.cpp

--- src/PlayerHook.cpp
+++ src/PlayerHook.cpp
@@ -5,17 +5,10 @@ namespace Hooks
 {
     bool PlayerHook::HookedIsInMidair(const RE::Actor* actor)
     {
-        if (!actor) {
-            spdlog::info("Actor is null");
-            return false;
-        }
-        spdlog::info("HookedIsInMidair called for actor: {}", actor->GetName());
-        // Custom logic: Always return false if the actor is the player
         if (actor->IsPlayerRef()) {
             spdlog::info("Actor is player, returning false for IsInMidair check");
             return false;
         }
-        // Call the original function for other actors
         return _IsInMidair(actor);
     }
 
